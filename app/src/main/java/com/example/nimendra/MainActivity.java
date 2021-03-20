b9268d6804b74889ca909e5789470f25dd372adf
NimendraK <nimendra.2019264@iit.ac.lk>
2021-03-21 01:38:40 +0530

    added essentials

--- app/src/main/java/com/example/nimendra/MainActivity.java
+++ app/src/main/java/com/example/nimendra/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.nimendra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Class name for Log tag
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // Hold the value of Switch Stats
+    boolean switchOn;
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwitchMaterial switch1 = (SwitchMaterial) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Get boolean value from parameter
+                switchOn = isChecked;
+            }
+        });
     }
 }
